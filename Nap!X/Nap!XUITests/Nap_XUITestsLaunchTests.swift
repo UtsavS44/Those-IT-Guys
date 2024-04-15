f16403d9d4eb6131780ee33caf00f21cfb0fcf64
Utsav Sharma <104206676+UtsavS44@users.noreply.github.com>
2024-04-15 09:35:33 +0530

    Add files via upload

---
+++ Nap!X/Nap!XUITests/Nap_XUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nap_XUITestsLaunchTests.swift
+//  Nap!XUITests
+//
+//  Created by Utsav  on 12/04/24.
+//
+
+import XCTest
+
+final class Nap_XUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
